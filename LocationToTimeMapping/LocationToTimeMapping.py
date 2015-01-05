c3ad67e6dd15889078df2245484bfe1990ed0008
Andriy Batutin <andriy.batutin@accenture.com>
2015-01-05 17:28:38 +0200

    converted time str to unix format

--- LocationToTimeMapping/LocationToTimeMapping.py
+++ LocationToTimeMapping/LocationToTimeMapping.py
@@ -1,7 +1,10 @@
-__author__ = 'andriy.batutin'
-from os import path
-from xml.dom.minidom import parse
+import xml
+from datetime import datetime
 import xml.dom.minidom
+import time
+import arrow
+__author__ = 'andriy.batutin'
+
 
 """
 1. build function from KML file. x -time, y - location
@@ -17,14 +20,14 @@ def parseKMLFile():
     DOMTree = xml.dom.minidom.parse("history-01-04-2015.kml")
     collection = DOMTree.documentElement
     track = collection.getElementsByTagName("gx:coord")
-    time = collection.getElementsByTagName("when")
+    timeDD = collection.getElementsByTagName("when")
     coordA = []
     timeA = []
     for element in track:
         coordA.append(element.childNodes[0].data)
-    for element in time:
-        timeA.append(element.childNodes[0].data)
-    res = []
-    for i in range(0, len(coordA)):
-        res.append((timeA[i], coordA[i]))
-    return res
+    for element in timeDD:
+        dateTimeStamp = arrow.get(element.childNodes[0].data)
+        timeA.append(dateTimeStamp.float_timestamp)
+
+    return (timeA, coordA)
+
